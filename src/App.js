0beabbfaeef25a7a07a19f70aa3897dca217e3dc
izeah <faizmohhafidza@gmail.com>
2021-07-23 11:06:40 +0700

    adding components, images, set tailwind, and display/hidden modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header.js";
+import Hero from "./components/Hero.js";
+import Browse from "./components/Browse.js";
+import Arrived from "./components/Arrived.js";
+import Clients from "./components/Clients.js";
+import AsideMenu from "./components/AsideMenu.js";
+import Footer from "./components/Footer.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header></Header>
+            <Hero></Hero>
+            <Browse></Browse>
+            <Arrived></Arrived>
+            <Clients></Clients>
+            <AsideMenu></AsideMenu>
+            <Footer></Footer>
+        </>
+    );
 }
 
 export default App;
